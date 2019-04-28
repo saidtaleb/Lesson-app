063db89af0365537dcb229111fe78b8fd5a4c90d
Taleb Said <saidtaleb128@gmail.com>
2019-04-28 22:15:19 +0100

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'UI/list.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Demo",
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(primaryColor: Color.fromRGBO(58, 66, 86, 1.0)),
+      home: new ListPage(title: 'Lessons'),
+    );
+  }
+}
